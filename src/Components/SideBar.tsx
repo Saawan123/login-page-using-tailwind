04aa6bd14d043d77609478352b7826c4daa9d027
Ankit Kumar <kumarankit1@seasiainfotech.com>
2024-01-25 18:50:17 +0530

    push code (25/01/2024)

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,9 +1,67 @@
 import { useState } from "react";
-import { hamburgerIcon } from "../assets/Icons/Icons";
-
+import { eyeButtonShow, hamburgerIcon } from "../assets/Icons/Icons";
+import { NavLink, useNavigate } from "react-router-dom";
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+//   const [toggle, setToggle] = useState(false);
+  const navigate = useNavigate();
 
+  const menu = [
+    {
+      name: "Dashboard",
+      icon:eyeButtonShow,
+   
+      submenu: [],
+      link: "",
+    },
+    {
+      icon:eyeButtonShow,
+      name: "Contact",
+    // //   icon: BuyersIcon,
+      link: "manage-listings",
+      submenu: [],
+    },
+    {
+      icon:eyeButtonShow,
+      name: "Services",
+    // //   icon: SellersIcon,
+      link: "manage-seller",
+      submenu: [],
+    },
+    {
+      name: "Settings",
+      icon:eyeButtonShow,
+
+    // //   icon: MarketingIcon,
+      link: "marketing",
+      submenu: [],
+    },
+    {
+      name: "Reports",
+      icon:eyeButtonShow,
+
+    // //   icon: ChartlineIcon,
+      link: "report",
+      submenu: [],
+    },
+    {
+      name: "Admins",
+      icon:eyeButtonShow,
+
+    // //   icon: settingIcon,
+      link: "admin-panel",
+      submenu: [],
+    },
+    {
+        name: "LogOut",
+        icon:eyeButtonShow,
+  
+      // //   icon: settingIcon,
+        link: "/login",
+        submenu: [],
+      }
+
+  ];
   return (
     <>
       {showSidebar ? (
@@ -27,9 +85,19 @@ const Sidebar = () => {
         }`}
       >
         <div className="top-0 left-0 w-[35vw] bg-green-600 p-10 pl-20 text-white fixed h-full ">
-          <h2 className="mt-20 text-4xl font-semibold text-white">
-            I am a sidebar
-          </h2>
+        <nav className="mt-10">
+        {menu.map((item:any, index:any) => (
+          <NavLink
+            key={index}
+            to={item.link}
+            className={({ isActive }) => `flex items-center mt-4 py-2 px-6 bg-opacity-25 text-gray-700 dark:text-gray-100 ${isActive ? 'bg-gray-200 dark:bg-gray-700' : ''}`}
+            onClick={item.onClick}
+          >
+            <span>{item.icon}</span>
+            <span>{item.name}</span>
+          </NavLink>
+        ))}
+      </nav>
         </div>
       </div>
     </>
